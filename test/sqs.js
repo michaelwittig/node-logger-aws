7a987ff6cfafca7d6b62677fc975bc17043e7edf
Michael Wittig <michael.wittig@tullius-walden.com>
2013-06-28 13:52:15 +0200

    added test

---
+++ test/sqs.js
@@ -0,0 +1,33 @@
+var assert = require("assert-plus"),
+	util = require("util"),
+	endpoint = require("../index"),
+	file = require("cinovo-logger-file");
+
+describe("sqs", function(){
+	describe("sqs()", function() {
+		it("should send an messag to a SNS topic", function(done) {
+			var log = {
+				level: "debug",
+				message: "Test",
+				origin: "test"
+			};
+			var e = endpoint.sqs(true, true, true, true, "eu-west-1", "queueUrl");
+			e.on("error", function(err) {
+				throw err;
+			});
+			e.log(log, function(err) {
+				if (err) {
+					throw err;
+				} else {
+					e.stop(function(err) {
+						if (err) {
+							throw err;
+						} else {
+							done();
+						}
+					});
+				}
+			});
+		});
+	});
+});
